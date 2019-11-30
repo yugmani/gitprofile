09011f742cc9916969f4c00ac3bb266fea024721
Himalaya <yugmani@yahoo.com>
2019-11-30 09:44:19 -0800

    final update

--- filepdf.js
+++ filepdf.js
@@ -1,6 +1,6 @@
 const puppeteer = require('puppeteer');
 const inquirer = require("inquirer");
-const fs = require('fs');
+
 
 inquirer
   .prompt({
@@ -8,76 +8,52 @@ inquirer
     name: "username"
   }).then(async({username})=> {
 
-//const createPdf = async()=>{
+                     
     try {
-        module.exports.username = username;
-    const browser = await puppeteer.launch({headless: true});
-   
-    const page = await browser.newPage();
+        
+          const browser = await puppeteer.launch({headless: true});
+          const page = await browser.newPage();
     
-
-    await page.emulateMediaType('screen');
-
-    
-
-    await page.content('./index.html', {waitUntil:'networkidle2'} );
-    
-    await page.waitForSelector('#searchInput');
-    
-    await page.type('#searchInput', `${username}`);
-    await page.waitForSelector('#inputImage');
-    await page.click('#inputImage', {clickCount: 3});
-
-    await page.waitForNavigation({waitUntil:'networkidle2'});
-    
-    //,
-
+                  const options = {
+                      
+                      path: 'Profile.pdf', 
+                      format: 'A4',
+                      printBackground: true
+                  };
 
     
-              const options = {
-                path: 'myProfile.pdf', 
-                format: 'Letter',
-                printBackground: true
-              };
+          await page.setViewport({width: 2300, height:1239});
+          
+          await page.goto('https://yugmani.github.io/gitprofile/', {waitUntil: 'networkidle2'});   
 
-    await page.pdf(options);
-       
-    console.log("Pdf is created. Success!");
-    await browser.close();
+              
+          await Promise.all([
+      
+                page.waitForSelector('#searchInput'),
+                page.focus( '#searchInput'),
+                
+                page.evaluate( () => document.getElementById("searchInput").value = ""),
+                page.evaluate((text) => {(document.getElementById('searchInput')).value = text; }, `${username}`, { delay: 1500 }),
+              
+               
+          ])
+            const button = ('#inputImage');
+            await page.waitForSelector('#inputImage'),
+            await page.focus( '#inputImage'),
+            await page.evaluate((button) => {document.getElementById('inputImage').click(); }, button);
+            
+            await page.click('#inputImage', { delay: 1500 }), 
+
+           
+          
+          await page.pdf(options);
+          console.log("Pdf! Success!!");
+          await browser.close();
   
-} catch(e){
+  } catch(e){
+        
     console.log("Here is error", e);
-}
+  }
     
 });
 
-// createPdf();
-
-
-//https://yugmani.github.io/gitprofile/
-// const fs = require('fs');
-// const path = require('path');
-
-//const username = 'fabpot';
-//process.argv[2];
-  // await page.removeListener('click' )
-    // await Promise.all([ 
-        
-    //     // page.waitForNavigation({waitUntil:'networkidle2'}),
-    //     //page.waitForNavigation(waitOptions),
-    //     page.click('#inputImage', {clickCount: 1}),
-    //     // page.click('#inputImage')
-    // ])
-
-    // await page.click('#inputImage')
-    // await page.waitForNavigation({waitUntil:'domcontentloaded'}),
-    
-    //await page.click('#inputImage');
-
-    // await Promise.all([
-    //     page.waitForNavigation(),
-    //     page.click('#inputImage')
-    // ]);
-    //await page.mouse.click('#inputImage', { delay: 1000 });
-
-    // await page.waitForNavigation();
--- gitprofile.js
+++ gitprofile.js
@@ -12,121 +12,68 @@ const stars = document.querySelector(".stars");
 const Tstars = document.querySelector(".Tstars");
 const blog = document.querySelector(".blog");
 const htmlUrl = document.querySelector(".gitUrl");
-// const location = res.data.location;
 
-// const puppeteer = require('puppeteer');
-// const fs = require('fs');
-// const path = require('path');
 
 const client_id = "Iv1.e2d89256ac13d5a2";
 const client_secret = "38270cbb1588a0abddf29e90afbd45cbf0e76ca1";
 
-const fetchUsers = async (user)=>{
-    const api_call = await fetch (`https://api.github.com/users/${user}?client_id=${client_id}&client_secret=${client_secret}`);
-   // const queUrl = await fetch (`https://api.github.com/users/${user}/repos`);
-    
-    const data = await api_call.json();
-    //const datas = await queUrl.json();
-    return {data};
-    // return ;
-};
+                const fetchUsers = async (user)=>{
+                    const api_call = await fetch (`https://api.github.com/users/${user}?client_id=${client_id}&client_secret=${client_secret}`);
+                // const queUrl = await fetch (`https://api.github.com/users/${user}/repos`);
+                    
+                    const data = await api_call.json();
+                    //const datas = await queUrl.json();
+                    return {data};
+                    // return ;
+                };
 
-const showData = () =>{
-    fetchUsers(searchInput.value).then((res)=>{
-        console.log(res);
-        name.innerHTML = `${res.data.name}`;
-        photo.setAttribute("src", `https://github.com/${res.data.login}.png?size=150`)
-        location.innerHTML = `Location: ${res.data.location}`;
-        repos.innerHTML = `Total Repos: ${res.data.public_repos}`;
-        followers.innerHTML = `Followers: ${res.data.followers}`;
-        following.innerHTML = `Following: ${res.data.following}`;
-        bio.innerHTML = `Bio: ${res.data.bio}`;
-        blog.innerHTML = `Blog: ${res.data.blog}`;
-        
-        htmlUrl.innerHTML = `Github Link: ${res.data.html_url}`;
-        
-        
+                    const showData = () =>{
+                        fetchUsers(searchInput.value).then((res)=>{
+                            console.log(res);
+                            name.innerHTML = `${res.data.name}`;
+                            photo.setAttribute("src", `https://github.com/${res.data.login}.png?size=150`)
+                            location.innerHTML = `Location: ${res.data.location}`;
+                            repos.innerHTML = `Total Repos: ${res.data.public_repos}`;
+                            followers.innerHTML = `Followers: ${res.data.followers}`;
+                            following.innerHTML = `Following: ${res.data.following}`;
+                            bio.innerHTML = `Bio: ${res.data.bio}`;
+                            blog.innerHTML = `Blog: ${res.data.blog}`;
+                            
+                            htmlUrl.innerHTML = `Github Link: ${res.data.html_url}`;
+                            
+                        })
+                    }
 
-
-    })
-}
-
-const techUsers = async (user)=>{
+    const techUsers = async (user)=>{
         const queUrl = await fetch (`https://api.github.com/users/${user}/repos`);
-       
-    const data = await queUrl.json();
-    return {data:data};
+        const data = await queUrl.json();
+        return {data:data};
 
-};
+    };
 
 
-const viewData = () =>{
-    techUsers(searchInput.value).then((res)=>{
-        // console.log(res);
-        // console.log(res.data[0].stargazers_count);
-        // const star_count = res.data[0].stargazers_count;
-        var starCount = 0;
-        
-        for (let i = 0; i < res.data.length; i++){
-          starCount +=  res.data[i].stargazers_count;
-          }
-        //   const mean = parseInt(starCount / res.data.length);
-         
-        //   console.log(mean);
-        //   console.log(starCount);
-        //   console.log(res.data.length);
-        //   stars.innerHTML = `Average Star: ${mean}`;
-          Tstars.innerHTML = `Total Star: ${starCount}`;
+                    const viewData = () =>{
+                        techUsers(searchInput.value).then((res)=>{
+                        
+                            var starCount = 0;
+                            
+                            for (let i = 0; i < res.data.length; i++){
+                            starCount +=  res.data[i].stargazers_count;
+                            }
+                        
+                            Tstars.innerHTML = `Total Star: ${starCount}`;
 
-    })
-    
-    }
+                        })
+                        
+                        }
 
 
 inputImage.addEventListener("click", ()=>{
-//box.style.display = "none";
+box.style.display = "none";
 
 showData();
 viewData();
 
 
 })
-
-
-
-
-// const createPdf = async()=>{
-//     try {
-
-//     const browser = await puppeteer.launch({headless: true});
-   
-//     const page = await browser.newPage();
-//     //const html = await fs.readFile('index.html', 'utf-8');
-
-//     const options = {
-//         path: 'myProfile.pdf', 
-//         format: 'A4',
-//         printBackground: true
-//     };
-
-//     await page.goto('https://yugmani.github.io/gitprofile/', { waitUntil:'networkidle2'});
-    
-//     await page.pdf(options);
-//     await page.emulateMediaType('screen');
-    
-
-//     console.log("done");
-   
-   
-//     await browser.close();
-//     // process.exit();
-
-// } catch(e){
-//     console.log("Here is error", e);
-// }
-    
-// };
-
-// // createPdf();
-
 
